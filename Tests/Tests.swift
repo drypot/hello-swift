dfe7684cf6b8a930fa1b44d833f4900af2658c6b
drypot <drypot@gmail.com>
2023-08-27 07:10:53 +0900

    add tests bundle

---
+++ Tests/Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by drypot on 2023-08-27.
+//
+
+import XCTest
+
+final class Tests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual("abc", "abc")
+    }
+
+}
