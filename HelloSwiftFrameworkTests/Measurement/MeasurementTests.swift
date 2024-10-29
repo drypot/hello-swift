5b821b4df0530af8c31ceedca6c19b3a54a16ceb
drypot <drypot@gmail.com>
2024-10-30 08:36:04 +0900

    update MeasurementTests

--- HelloSwiftFrameworkTests/Measurement/MeasurementTests.swift
+++ HelloSwiftFrameworkTests/Measurement/MeasurementTests.swift
@@ -8,20 +8,31 @@
 import Foundation
 import Testing
 
+// https://developer.apple.com/documentation/foundation/measurement
+// https://developer.apple.com/documentation/foundation/unit
+// https://developer.apple.com/documentation/foundation/dimension
+
 struct MeasurementTests {
 
-    @Test func test() throws {
-        let m = Measurement(value: 90, unit: UnitDuration.seconds)
+    @Test func testUnitDuration() throws {
+        let m1 = Measurement(value: 90, unit: UnitDuration.seconds)
+        let m2 = m1.converted(to: .minutes)
 
-        #expect(m.value == 90)
-        #expect(m.unit == .seconds)
-        #expect(m.converted(to: .minutes).value == 1.5)
+        #expect(m1.value == 90)
+        #expect(m1.unit == .seconds)
 
-        // TO DO
-//        var style = Duration.UnitsFormatStyle(
-//        style.locale = Locale(identifier: "en_US_POSIX")
-//        #expect(m.formatted(style) == "1.5 minutes")
+        #expect(m2.value == 1.5)
+        #expect(m2.unit == .minutes)
 
+        #expect(m1 == m2)
+    }
+
+    @Test func testArithmetic() throws {
+        let m1 = Measurement(value: 90, unit: UnitDuration.seconds)
+        let m2 = Measurement(value: 1, unit: UnitDuration.minutes)
+
+        #expect(m1 + m2 == Measurement(value: 150, unit: .seconds))
+        #expect(m1 - m2 == Measurement(value: 30, unit: .seconds))
     }
 
 }
