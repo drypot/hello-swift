8e3d3e610c801355db077f80d04d1c3ec029f6d3
drypot <drypot@gmail.com>
2024-11-01 03:57:21 +0900

    rearrange folders

--- HelloSwiftFrameworkTests/Collections/Sequence/ListFormatStyleTests.swift
+++ HelloSwiftFrameworkTests/Collections/Sequence/ListFormatStyleTests.swift
@@ -2,7 +2,7 @@
 //  ListFormatStyleTests.swift
 //  HelloSwiftFrameworkTests
 //
-//  Created by Kyuhyun Park on 10/29/24.
+//  Created by Kyuhyun Park on 11/1/24.
 //
 
 import Foundation
@@ -10,7 +10,7 @@ import Testing
 
 struct ListFormatStyleTests {
 
-    @Test func test() throws {
+    @Test func tests() throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
---
+++ HelloSwiftFrameworkTests/Collections/Sequence/SequenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SequenceTests.swift
+//  HelloSwiftFrameworkTests
+//
+//  Created by Kyuhyun Park on 11/1/24.
+//
+
+import Foundation
+import Testing
+
+struct SequenceTests {
+
+    @Test func test() throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
