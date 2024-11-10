1e6cd43ca63f0fdb555761289cc9035f9096c04e
drypot <drypot@gmail.com>
2024-11-11 02:13:53 +0900

    update DurationTests

--- HelloSwiftFrameworkTests/Values/DateTime/Duration/DurationTests.swift
+++ HelloSwiftFrameworkTests/Values/DateTime/Duration/DurationTests.swift
@@ -29,11 +29,13 @@ struct DurationTests {
 
         let sum = seconds + milliseconds + microseconds + nanoseconds
 
-        // Duration 만들 때 float 쓰지말자.
-
-        #expect(sum == Duration.nanoseconds(55_066_077_088))
-        #expect(sum != Duration.seconds(55.066_077_088)) // 다르다고 나온다.
+        // Duration 만들 때 Double 이나 BinaryInteger 를 쓸 수 있다.
+        // Double 을 써서 소숫점 아래 수치들을 사용하면 오차가 발생한다.
+        #expect(sum != Duration.seconds(55.066_077_088))
         #expect(sum < Duration.seconds(55.066_077_088))
+
+        // 단위를 내려서 정수로 표현하면 오차가 사라진다.
+        #expect(sum == Duration.nanoseconds(55_066_077_088))
 
         #expect(Duration.seconds(0.100) == Duration.milliseconds(100))
         #expect(Duration.seconds(0.444) == Duration.milliseconds(444))
