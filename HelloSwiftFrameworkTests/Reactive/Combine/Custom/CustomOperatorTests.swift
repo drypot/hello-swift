5c150cbe40fdbcb64c791fab1655fe1ba127227e
drypot <drypot@gmail.com>
2024-12-16 22:56:09 +0900

    add HandleEventsTests

--- HelloSwiftFrameworkTests/Reactive/Combine/Custom/CustomOperatorTests.swift
+++ HelloSwiftFrameworkTests/Reactive/Combine/Custom/CustomOperatorTests.swift
@@ -20,11 +20,13 @@ struct CustomOperatorTests {
         let upstream: Upstream
         let transform: (Upstream.Output) -> Output?
 
+        // 1차, 위에서 아래로 Publisher, Operator 들이 만들어 진다.
         init(upstream: Upstream, transform: @escaping (Upstream.Output) -> Output) {
             self.upstream = upstream
             self.transform = transform
         }
 
+        // 2차, 아래서 위로 하위 Subscription 을 상위 Subscription 연결한다.
         func receive<S>(subscriber: S)
         where S: Subscriber, Output == S.Input, Failure == S.Failure {
             let subscription = CustomOperatorSubscription(
@@ -47,15 +49,18 @@ struct CustomOperatorTests {
             self.transform = transform
         }
 
+        // 3차, 위에서 아래로 상위 Subscription 을 하위 Subscription 에 연결한다.
         func receive(subscription: Subscription) {
             self.subscription = subscription
             subscriber?.receive(subscription: self)
         }
 
+        // 4차, 아래에서 위로 request 를 올려보낸다.
         func request(_ demand: Subscribers.Demand) {
             subscription?.request(demand)
         }
 
+        // 5차, 위에서 아래로 value 를 내려보낸다.
         func receive(_ input: Input) -> Subscribers.Demand {
             guard let value = transform(input) else { return .none }
             return subscriber?.receive(value) ?? .none
---
+++ HelloSwiftFrameworkTests/Reactive/Combine/Operators/HandleEventsTests.swift
@@ -0,0 +1,52 @@
+//
+//  HandleEventsTests.swift
+//  HelloSwift
+//
+//  Created by Kyuhyun Park on 12/16/24.
+//
+
+import Foundation
+import Combine
+import Testing
+
+// https://developer.apple.com/documentation/combine
+
+struct HandleEventsTests {
+
+    @Test func test() throws {
+        let logger = SimpleLogger<String>()
+
+        let _ = Just("value")
+
+            .handleEvents(receiveSubscription: { _ in
+                logger.append("receiveSubscription")
+            }, receiveOutput: { _ in
+                logger.append("receiveOutput")
+            }, receiveCompletion: { _ in
+                logger.append("receiveCompletion")
+            }, receiveCancel: {
+                logger.append("receiveCancel")
+            }, receiveRequest: { _ in
+                logger.append("receiveRequest")
+            })
+
+            .sink { completion in
+                logger.append("sink completion")
+            } receiveValue: { value in
+                logger.append("sink value")
+            }
+
+        logger.append("end")
+
+        #expect(logger.log() == [
+            "receiveSubscription",
+            "receiveRequest",
+            "receiveOutput",
+            "sink value",
+            "receiveCompletion",
+            "sink completion",
+            "end"
+        ])
+    }
+
+}
