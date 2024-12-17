7bcd3892581fb2e00f7ae0825019d8f44ebda744
drypot <drypot@gmail.com>
2024-12-17 20:57:03 +0900

    add RunLoopTests

---
+++ HelloSwiftFrameworkTests/Foundation/Concurrency/RunLoop/RunLoopTests.swift
@@ -0,0 +1,23 @@
+//
+//  RunLoopTests.swift
+//  HelloSwiftFrameworkTests
+//
+//  Created by Kyuhyun Park on 12/17/24.
+//
+
+import Foundation
+import Testing
+
+struct RunLoopTests {
+
+    @Test func test() throws {
+
+        // 콘솔 어플리케이션 만들어서 백그라운드 프로세스 시작시킨 후
+        // 프로세스 종료시키지 않고 멍때리게 만들고 싶다면,
+        // RunLoop 가동시키는 것이 무난해 보인다.
+
+        // RunLoop.current.run()
+        
+    }
+
+}
