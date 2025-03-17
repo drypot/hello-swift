a6ce5f3992df40b9ad27e8f75265b28f1bfad1bc
drypot <drypot@gmail.com>
2025-03-17 10:11:51 +0900

    update DateFormat

--- HelloSwiftFrameworkTests/Foundation/Formatting/Date/DateInterval/DateIntervalFormatTests.swift
+++ HelloSwiftFrameworkTests/Foundation/Formatting/Date/DateInterval/DateIntervalFormatTests.swift
@@ -15,75 +15,65 @@ struct DateIntervalFormatTests {
     let date = Date(timeIntervalSinceReferenceDate: 751451410.0)
 
     @Test func testFactoryVariable() throws {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.locale = Locale(identifier: "ko_KR")
-        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
+        let calendar = Calendar(identifier: .gregorian)
 
-        let next30days = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+        let range = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
 
-        #expect(next30days.formatted(.interval) == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
-        #expect(next30days.formatted(.interval.month(.wide).day()) == "10월 24일 ~ 11월 23일")
+        // https://developer.apple.com/documentation/foundation/date/intervalformatstyle/3796299-interval
+        // interval: A convenience factory variable to use as a base for custom date interval format styles.
+
+        #expect(range.formatted(.interval) == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
+        #expect(range.formatted(.interval.month(.wide).day()) == "10월 24일 ~ 11월 23일")
     }
 
     @Test func testPredefined() throws {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.locale = Locale(identifier: "ko_KR")
-        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
+        let calendar = Calendar(identifier: .gregorian)
 
-        let next30days = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+        let range = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
 
-        #expect(next30days.formatted() == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
+        #expect(range.formatted() == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
 
-        #expect(next30days.formatted(date: .numeric, time: .omitted)     == "2024. 10. 24. ~ 2024. 11. 23.")
-        #expect(next30days.formatted(date: .abbreviated, time: .omitted) == "2024년 10월 24일 ~ 11월 23일")
-        #expect(next30days.formatted(date: .long, time: .omitted)        == "2024년 10월 24일 ~ 11월 23일")
-        #expect(next30days.formatted(date: .complete, time: .omitted)    == "2024년 10월 24일 목요일 ~ 11월 23일 토요일")
+        #expect(range.formatted(date: .numeric, time: .omitted)     == "2024. 10. 24. ~ 2024. 11. 23.")
+        #expect(range.formatted(date: .abbreviated, time: .omitted) == "2024년 10월 24일 ~ 11월 23일")
+        #expect(range.formatted(date: .long, time: .omitted)        == "2024년 10월 24일 ~ 11월 23일")
+        #expect(range.formatted(date: .complete, time: .omitted)    == "2024년 10월 24일 목요일 ~ 11월 23일 토요일")
 
-        #expect(next30days.formatted(date: .omitted, time: .shortened)   == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
-        #expect(next30days.formatted(date: .omitted, time: .standard)    == "2024. 10. 24. 오후 5:30:10 ~ 2024. 11. 23. 오후 5:30:10")
-        #expect(next30days.formatted(date: .omitted, time: .complete)    == "2024. 10. 24. 오후 5시 30분 10초 GMT+9 ~ 2024. 11. 23. 오후 5시 30분 10초 GMT+9")
+        #expect(range.formatted(date: .omitted, time: .shortened)   == "2024. 10. 24. 오후 5:30 ~ 2024. 11. 23. 오후 5:30")
+        #expect(range.formatted(date: .omitted, time: .standard)    == "2024. 10. 24. 오후 5:30:10 ~ 2024. 11. 23. 오후 5:30:10")
+        #expect(range.formatted(date: .omitted, time: .complete)    == "2024. 10. 24. 오후 5시 30분 10초 GMT+9 ~ 2024. 11. 23. 오후 5시 30분 10초 GMT+9")
 
-        let next3hours = date ..< calendar.date(byAdding: .hour, value: 3, to: date)!
+        let range2 = date ..< calendar.date(byAdding: .hour, value: 3, to: date)!
 
-        #expect(next3hours.formatted(date: .omitted, time: .shortened)   == "오후 5:30~8:30")
-        #expect(next3hours.formatted(date: .omitted, time: .standard)    == "오후 5:30:10 ~ 오후 8:30:10")
-        #expect(next3hours.formatted(date: .omitted, time: .complete)    == "오후 5시 30분 10초 GMT+9 ~ 오후 8시 30분 10초 GMT+9")
+        #expect(range2.formatted(date: .omitted, time: .shortened)   == "오후 5:30~8:30")
+        #expect(range2.formatted(date: .omitted, time: .standard)    == "오후 5:30:10 ~ 오후 8:30:10")
+        #expect(range2.formatted(date: .omitted, time: .complete)    == "오후 5시 30분 10초 GMT+9 ~ 오후 8시 30분 10초 GMT+9")
     }
 
     @Test func testPredefined2() throws {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.locale = Locale(identifier: "ko_KR")
-        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
-
-        let next30days = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+        let calendar = Calendar(identifier: .gregorian)
 
         var style = Date.IntervalFormatStyle(date: .abbreviated, time: .shortened)
         style.calendar = calendar
         style.locale = Locale(identifier: "ko_KR")
         style.timeZone = TimeZone(identifier: "Asia/Seoul")!
 
-        #expect(next30days.formatted(style) == "2024년 10월 24일 오후 5:30 ~ 2024년 11월 23일 오후 5:30")
-        #expect(next30days.formatted(style) == next30days.formatted(date: .abbreviated, time: .shortened))
+        let range = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+
+        #expect(range.formatted(style) == "2024년 10월 24일 오후 5:30 ~ 2024년 11월 23일 오후 5:30")
     }
 
     @Test func testModifiers() throws {
-        var calendar = Calendar(identifier: .gregorian)
-        calendar.locale = Locale(identifier: "ko_KR")
-        calendar.timeZone = TimeZone(identifier: "Asia/Seoul")!
-
-        let next30days = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+        let calendar = Calendar(identifier: .gregorian)
 
         var style = Date.IntervalFormatStyle()
-            .month(.wide)
-            .day()
-            .weekday(.short)
-            .hour(.conversationalTwoDigits(amPM: .wide))
-
+            .month(.wide).day().weekday(.short).hour(.conversationalTwoDigits(amPM: .wide))
         style.calendar = calendar
         style.locale = Locale(identifier: "ko_KR")
         style.timeZone = TimeZone(identifier: "Asia/Seoul")!
 
-        #expect(next30days.formatted(style) == "10월 24일 (목) 오후 5시 ~ 11월 23일 (토) 오후 5시")
+        let range = date ..< calendar.date(byAdding: .day, value: 30, to: date)!
+
+        #expect(range.formatted(style) == "10월 24일 (목) 오후 5시 ~ 11월 23일 (토) 오후 5시")
     }
 
 }
--- HelloSwiftFrameworkTests/Foundation/Formatting/Date/ISO8601/ISO8601FormatTests.swift
+++ HelloSwiftFrameworkTests/Foundation/Formatting/Date/ISO8601/ISO8601FormatTests.swift
@@ -12,53 +12,47 @@ struct ISO8601FormatTests {
 
     // 2024-10-24 08:30:10 +0000
     // 2024-10-24 17:30:10 +0900
-    let date = Date(timeIntervalSinceReferenceDate: 751451410.0)
+    let date1410 = Date(timeIntervalSinceReferenceDate: 751451410.0)
 
     @Test func testFactoryVariable() throws {
-        #expect(date.formatted(.iso8601) == "2024-10-24T08:30:10Z")
+
+        // https://developer.apple.com/documentation/foundation/date/iso8601formatstyle/3796293-iso8601
+        // iso8601: A convenience factory variable that provides a base format for customizing ISO 8601 date format styles.
+
+        #expect(date1410.formatted(.iso8601) == "2024-10-24T08:30:10Z")
     }
 
     @Test func testDefaults() throws {
         var style = Date.ISO8601FormatStyle()
         style.timeZone = TimeZone(identifier: "Asia/Seoul")!
 
-        let string1 = date.formatted(style)
-        let string2 = style.format(date)
-
-        #expect(string1 == string2)
-        #expect(string1 == "2024-10-24T17:30:10+0900")
+        #expect(style.format(date1410) == "2024-10-24T17:30:10+0900")
+        #expect(date1410.formatted(style) == "2024-10-24T17:30:10+0900")
     }
 
     @Test func testModifiers() throws {
         var style = Date.ISO8601FormatStyle()
-            .year().month().day().time(includingFractionalSeconds: false)
-            .dateTimeSeparator(.space)
-
+            .year().month().day().time(includingFractionalSeconds: false).dateTimeSeparator(.space)
         style.timeZone = TimeZone(identifier: "Asia/Seoul")!
 
-        let string1 = date.formatted(style)
-        let string2 = style.format(date)
-
-        #expect(string1 == string2)
-        #expect(string1 == "2024-10-24 17:30:10")
+        #expect(style.format(date1410) == "2024-10-24 17:30:10")
+        #expect(date1410.formatted(style) == "2024-10-24 17:30:10")
     }
 
     @Test func testParsing() throws {
         var style = Date.ISO8601FormatStyle()
-            .year().month().day().time(includingFractionalSeconds: false)
-            .dateTimeSeparator(.space)
-
+            .year().month().day().time(includingFractionalSeconds: false).dateTimeSeparator(.space)
         style.timeZone = TimeZone(identifier: "Asia/Seoul")!
 
-        let string1 = date.formatted(style)
-        let string2 = style.format(date)
+        let string1 = date1410.formatted(style)
 
-        #expect(string1 == string2)
         #expect(string1 == "2024-10-24 17:30:10")
 
-        let parsedDate = try style.parse(string1)
+        let date1 = try! Date(string1, strategy: style)
+        let date2 = try! style.parse(string1)
 
-        #expect(parsedDate == date)
+        #expect(date1 == date1410)
+        #expect(date2 == date1410)
     }
 
 }
