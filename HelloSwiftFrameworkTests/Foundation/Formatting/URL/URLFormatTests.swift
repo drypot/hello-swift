60d454e7d36c8321b6c434bc7aa320ff72b69494
drypot <drypot@gmail.com>
2025-03-18 10:57:49 +0900

    add URLFormatTests

---
+++ HelloSwiftFrameworkTests/Foundation/Formatting/URL/URLFormatTests.swift
@@ -0,0 +1,57 @@
+//
+//  URLFormatTests.swift
+//  HelloSwiftFrameworkTests
+//
+//  Created by Kyuhyun Park on 3/18/25.
+//
+
+import Foundation
+import Testing
+
+// Data Formatting
+// https://developer.apple.com/documentation/foundation/data_formatting
+
+// URL.FormatStyle
+// https://developer.apple.com/documentation/foundation/url/formatstyle
+
+struct URLFormatTests {
+
+    @Test func testFactoryVariables() throws {
+
+        // https://developer.apple.com/documentation/foundation/formatstyle/4013379-url
+        // url: A style for formatting a URL.
+
+        let url = URL(string: "http://www.example.com:8080/path/to/file.txt")!
+
+        let formatted = url.formatted(
+            .url
+            .scheme(.never)
+            .host(.omitSpecificSubdomains(["www"]))
+            .port(.never)
+        )
+
+        #expect(formatted == "example.com/path/to/file.txt")
+    }
+
+    @Test func testStyle() throws {
+        let style = URL.FormatStyle(
+            scheme: .always,
+            user: .never,
+            password: .never,
+            host: .always,
+            port: .always,
+            path: .always,
+            query: .never,
+            fragment: .never
+        )
+
+        let url = URL(string:"https://www.example.com:8080/path/to/endpoint?key=value")!
+
+        let formatted1 = style.format(url)
+        let formatted2 = url.formatted(style)
+
+        #expect(formatted1 == "https://www.example.com:8080/path/to/endpoint")
+        #expect(formatted2 == "https://www.example.com:8080/path/to/endpoint")
+    }
+
+}
